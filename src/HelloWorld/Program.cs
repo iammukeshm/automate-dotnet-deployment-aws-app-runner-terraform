a2d85d5de28822b78b755f2953604c125d327a2e
Mukesh Murugan <iammukeshm@gmail.com>
2025-08-16 13:58:42 +0530

    update

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () =>
 {
-    return TypedResults.Ok("Hello World - From Mukesh");
+    return TypedResults.Ok("Hello World");
 })
 .WithName("HelloWorld");
 
